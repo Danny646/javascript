1f5c8de4eb4c4f806fa911657f513d3d49b2404a
admin <admin@admins-Air.attlocal.net>
2020-04-06 23:36:45 -0700

    created javascript files

---
+++ bands.js
@@ -0,0 +1,20 @@
+
+<style> 
+
+</style>
+ 
+ <script> 
+const bands = ['The Pilot', 'The Devil Wears Prada', 'Pierce the Viel', 'Norman Jean', 'The Bled', 'Say Anything', 'The Miday State', 'We Came as Romans', 'Counterparts', 'Oh Sleeper', 'A Skylift Driver', 'Anywhere But Here', 'An Old Dog',]; 
+function strip(bandName) {
+ return bandName.replace(/^(a|the)i)
+}
+</script>
+const sortedBands = bands.sort(function(a,b){;
+if (a>b){
+ returni;
+
+} else {
+  return-1;  
+}
+});
+console.log(sortedBands);
